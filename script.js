0858b29700e017e312459abed711c4635a3971a6
karthik <50404494+karthikrock123@users.noreply.github.com>
2020-01-22 22:23:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Get the mysql service
+var mysql = require('mysql');
+
+// Add the credentials to access your database
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'testDB'
+});
+
+// connect to mysql
+connection.connect(function(err) {
+    // in case of error
+    if(err){
+        console.log(err.code);
+        console.log(err.fatal);
+    }
+});
+
+// Perform a query
+$query = 'SELECT * from mySampleTable LIMIT 10';
+
+connection.query($query, function(err, rows, fields) {
+    if(err){
+        console.log("An error ocurred performing the query.");
+        return;
+    }
+
+    console.log("Query succesfully executed: ", rows);
+    
+});
+
+// Close the connection
+connection.end(function(){
+    // The connection has been closed
+});
